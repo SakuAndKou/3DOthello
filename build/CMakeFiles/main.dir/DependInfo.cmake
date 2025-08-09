eb40472045ebf4556a91debe2a2270f675d6080b
sakusakuankomochi <koutanmanz@outlook.jp>
2025-08-10 08:25:41 +0900

    Edit README

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/programming/C++/NextDXLIB/src/Manager.cpp" "CMakeFiles/main.dir/src/Manager.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Manager.cpp.obj.d"
+  "D:/programming/C++/NextDXLIB/src/Othello/3DOthello.cpp" "CMakeFiles/main.dir/src/Othello/3DOthello.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Othello/3DOthello.cpp.obj.d"
+  "D:/programming/C++/NextDXLIB/src/Othello/OthelloBoard.cpp" "CMakeFiles/main.dir/src/Othello/OthelloBoard.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Othello/OthelloBoard.cpp.obj.d"
+  "D:/programming/C++/NextDXLIB/src/Select/Select.cpp" "CMakeFiles/main.dir/src/Select/Select.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Select/Select.cpp.obj.d"
+  "D:/programming/C++/NextDXLIB/src/Start/Start.cpp" "CMakeFiles/main.dir/src/Start/Start.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Start/Start.cpp.obj.d"
+  "D:/programming/C++/NextDXLIB/src/VECTOR.cpp" "CMakeFiles/main.dir/src/VECTOR.cpp.obj" "gcc" "CMakeFiles/main.dir/src/VECTOR.cpp.obj.d"
+  "D:/programming/C++/NextDXLIB/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Manager.cpp.obj"
+  "CMakeFiles/main.dir/src/Manager.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Othello/3DOthello.cpp.obj"
+  "CMakeFiles/main.dir/src/Othello/3DOthello.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Othello/OthelloBoard.cpp.obj"
+  "CMakeFiles/main.dir/src/Othello/OthelloBoard.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Select/Select.cpp.obj"
+  "CMakeFiles/main.dir/src/Select/Select.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Start/Start.cpp.obj"
+  "CMakeFiles/main.dir/src/Start/Start.cpp.obj.d"
+  "CMakeFiles/main.dir/src/VECTOR.cpp.obj"
+  "CMakeFiles/main.dir/src/VECTOR.cpp.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
